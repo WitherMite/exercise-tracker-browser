0f05224a01ed052adf07a2f62527d471442538f5
WitherMite <43625516+WitherMite@users.noreply.github.com>
2026-07-27 23:39:37 -0500

    Use navlinks in nav to show active view

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,17 +1,32 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import style from "./Navbar.module.css";
 
+const links = [
+    {
+        path: "/",
+        childNode: "Landing",
+    },
+    {
+        path: "/home",
+        childNode: "Dashboard",
+    },
+] as const;
+
 export default function Navbar() {
+    // should be fine to use index as key here, since we control the array, and can ensure it wont change after building
+    const linkElements: ReadonlyArray<React.JSX.Element> = links.map(
+        (link, i) => (
+            <li key={i}>
+                <NavLink to={link.path} className={style.navLink}>
+                    {link.childNode}
+                </NavLink>
+            </li>
+        ),
+    );
+
     return (
         <nav className={style.navTrack}>
-            <ul className={style.navBar}>
-                <li>
-                    <Link to="/">Landing</Link>
-                </li>
-                <li>
-                    <Link to="/home">Dashboard</Link>
-                </li>
-            </ul>
+            <ul className={style.navBar}>{linkElements}</ul>
         </nav>
     );
 }
